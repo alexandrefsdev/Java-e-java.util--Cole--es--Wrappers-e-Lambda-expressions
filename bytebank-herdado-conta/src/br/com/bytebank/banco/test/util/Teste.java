643c5073d5f26c7e7522ba2d92c5abc1493b827a
Alexandre Silveira <alexandrefsdev@gmail.com>
2022-03-13 17:41:11 -0300

    Metodos e mais listas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -9,43 +9,40 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		ArrayList lista = new ArrayList();
+        ArrayList<Conta> lista = new ArrayList<Conta>();
 
-		
-		Conta cc =  new ContaCorrente(45, 50);
+        Conta cc = new ContaCorrente(22, 11);
+        lista.add(cc);
 
-		lista.add(cc);
-		
-		Conta cc2 =  new ContaCorrente(45, 2002);
-		lista.add(cc2);
-		
-		System.out.println(lista.size());
-		Conta ref = (Conta) lista.get(0);
-		System.out.println(ref.getNumero());
-		
-		lista.remove(0);
-		
-		System.out.println("Tamanho: " + lista.size());
-		
-		
-		Conta cc3 =  new ContaCorrente(45, 2015);
-		lista.add(cc3);
-		
-		Conta cc4 =  new ContaCorrente(45, 3525);
-		lista.add(cc4);
-		
-		for(int i = 0; i < lista.size(); i++) {
-			Object oRef = lista.get(i);
-			System.out.println(oRef);
-		}
-		
-		System.out.println("----------------");
-		
-		for(Object oRef : lista) {
-			System.out.println(oRef);
-		}
-		
-		
+        Conta cc2 = new ContaCorrente(22, 22);
+        lista.add(cc2);
+
+        System.out.println("Tamanho: " + lista.size());
+
+        Conta ref = lista.get(0);
+        System.out.println(ref.getNumero());
+
+        lista.remove(0);
+        System.out.println("Tamanho: " + lista.size());
+
+        Conta cc3 = new ContaCorrente(33, 311);
+        lista.add(cc3);
+
+        Conta cc4 = new ContaCorrente(33, 322);
+        lista.add(cc4);
+
+        for(int i = 0; i < lista.size(); i++) {
+            Object oRef = lista.get(i);
+            System.out.println(oRef);
+        }
+
+        System.out.println("----------");
+
+        for(Conta oRef : lista) {
+            System.out.println(oRef);
+        }
+
+  
 		
 	}
 
