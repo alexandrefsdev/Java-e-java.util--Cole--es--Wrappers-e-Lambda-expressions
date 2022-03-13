7c88f77566c250ce1e2300c77b374fc320789682
Alexandre Silveira <alexandrefsdev@gmail.com>
2022-03-13 18:26:27 -0300

    contains e equals

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -92,6 +92,23 @@ public abstract class Conta extends Object {
         return Conta.total;
     }
     
+
+	@Override
+	public boolean equals(Object ref) {
+		
+		Conta outraConta = (Conta) ref;
+		
+        if(this.agencia != outraConta.agencia) {
+            return false;
+        }
+
+        if(this.numero != outraConta.numero) {
+            return false;
+        }
+
+        return true;
+	}
+    
     @Override
 	public String toString() {
 		return "Numero: " + this.numero + ", Agencia: " + this.agencia;
